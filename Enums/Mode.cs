df227670c1b2a01db9165d1855c64a89ac828c56
jiniux <53437303+jiniux@users.noreply.github.com>
2021-06-20 23:47:54 +0200

    Stats and enums.

---
+++ Enums/Mode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oldsu.Enums
+{
+    public enum Mode
+    {
+        Standard,
+        Taiko,
+        CatchTheBeat,
+        Mania
+    }
+}
---
+++ Enums/Version.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oldsu.Enums
+{
+    enum Version
+    {
+        B394A
+    }
+}
