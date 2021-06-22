9e7ad0b8d0d83880f301004b73f936f19f6c20df
jeesusmies <burnerthingxd@gmail.com>
2021-06-22 14:20:06 +0300

    add login enums

---
+++ oldsu/Enums/LoginResult.cs
@@ -0,0 +1,13 @@
+﻿namespace osuserver2012.Enums
+{
+    public enum LoginResult
+    {
+        TestBuildButNotSupporter = -6,
+        ServerSideError = -5,
+        AccountNotActivated = -4,
+        Banned = -3,
+        TooOldVersion = -2,
+        AuthenticationFailed = -1,
+        AuthenticationSuccessful = 0
+    }
+}
