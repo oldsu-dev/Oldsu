6af7a0b31da3edffac50bd42bef5dc5530cca361
jeesusmies <burnerthingxd@gmail.com>
2021-07-14 14:20:32 +0300

    Add locks to ensure total safety and a Values variable.

--- oldsu/Utils/MultiKeyConcurrentDictionary.cs
+++ oldsu/Utils/MultiKeyConcurrentDictionary.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Dynamic;
 
 namespace Oldsu.Utils
 {
@@ -10,24 +12,38 @@ namespace Oldsu.Utils
         private ConcurrentDictionary<TKey1, TValue> dict1 = new();
         private ConcurrentDictionary<TKey2, TValue> dict2 = new();
 
+        public ICollection<TValue> Values => dict1.Values;
+
         public bool TryGetValue(TKey1 key1, out TValue value)
         {
-            return dict1.TryGetValue(key1, out value);
+            lock (this)
+            {
+                return dict1.TryGetValue(key1, out value);
+            }
         }
 
         public bool TryGetValue(TKey2 key2, out TValue value)
         {
-            return dict2.TryGetValue(key2, out value);
+            lock (this)
+            {
+                return dict2.TryGetValue(key2, out value);
+            }
         }
 
         public bool TryAdd(TKey1 key1, TKey2 key2, TValue value)
         {
-            return dict1.TryAdd(key1, value) && dict2.TryAdd(key2, value);
+            lock (this)
+            {
+                return dict1.TryAdd(key1, value) && dict2.TryAdd(key2, value);
+            }
         }
 
         public bool TryRemove(TKey1 key1, TKey2 key2, TValue value)
         {
-            return dict1.TryRemove(key1, out _) && dict2.TryRemove(key2, out _);
+            lock (this)
+            {
+                return dict1.TryRemove(key1, out _) && dict2.TryRemove(key2, out _);
+            }
         }
     }
 }
