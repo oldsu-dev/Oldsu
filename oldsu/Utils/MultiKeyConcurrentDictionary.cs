668b38a685590f7afc9b44fce3c30b3a73bab449
Jiniux <53437303+j1niux@users.noreply.github.com>
2021-07-15 13:21:08 +0200

    Optimized MultiKeyConcurrentDictionary

--- oldsu/Utils/MultiKeyConcurrentDictionary.cs
+++ oldsu/Utils/MultiKeyConcurrentDictionary.cs
@@ -1,48 +1,122 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Dynamic;
+using System.Threading;
 
 namespace Oldsu.Utils
 {
+    public class MultiKeyConcurrentDictionaryValuesGuard<TValue> : IEnumerable<TValue>, IDisposable
+    {
+        private readonly ReaderWriterLockSlim _rwLock;
+        private readonly IEnumerator<TValue> _enumerable;
+
+        public MultiKeyConcurrentDictionaryValuesGuard(ReaderWriterLockSlim rwLock, IEnumerator<TValue> enumerable)
+        {
+            _rwLock = rwLock;
+            _rwLock.EnterReadLock();
+
+            _enumerable = enumerable;
+        }
+
+        public IEnumerator<TValue> GetEnumerator()
+        {
+            return _enumerable;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private bool _disposed = false;
+        
+        public void Dispose()
+        {
+            if (_disposed) return;
+            
+            _rwLock.ExitReadLock();
+            _disposed = true;
+            
+            GC.SuppressFinalize(this);
+        }
+    }
+    
     public class MultiKeyConcurrentDictionary<TKey1, TKey2, TValue>
         where TKey1 : notnull
         where TKey2 : notnull
         where TValue : notnull
     {
-        private ConcurrentDictionary<TKey1, TValue> dict1 = new();
-        private ConcurrentDictionary<TKey2, TValue> dict2 = new();
+        public MultiKeyConcurrentDictionary()
+        {
+            _dict1 = new Dictionary<TKey1, TValue>();
+            _dict2 = new Dictionary<TKey2, TValue>();
 
-        public ICollection<TValue> Values => dict1.Values;
+            _rwLock = new ReaderWriterLockSlim();
+        }
+
+        public MultiKeyConcurrentDictionaryValuesGuard<TValue> Values => 
+            new(_rwLock, _dict1.Values.GetEnumerator());
+
+        private readonly ReaderWriterLockSlim _rwLock;
+        
+        private readonly Dictionary<TKey1, TValue> _dict1;
+        private readonly Dictionary<TKey2, TValue> _dict2;
 
         public bool TryGetValue(TKey1 key1, out TValue value)
         {
-            lock (this)
+            _rwLock.EnterReadLock();
+
+            try
             {
-                return dict1.TryGetValue(key1, out value);
+                return _dict1.TryGetValue(key1, out value);
+            }
+            finally
+            {
+                _rwLock.ExitReadLock();
             }
         }
 
         public bool TryGetValue(TKey2 key2, out TValue value)
         {
-            lock (this)
+            _rwLock.EnterReadLock();
+
+            try
             {
-                return dict2.TryGetValue(key2, out value);
+                return _dict2.TryGetValue(key2, out value);
+            }
+            finally
+            {
+                _rwLock.ExitReadLock();
             }
         }
 
         public bool TryAdd(TKey1 key1, TKey2 key2, TValue value)
         {
-            lock (this)
+            _rwLock.EnterWriteLock();
+
+            try
             {
-                return dict1.TryAdd(key1, value) && dict2.TryAdd(key2, value);
+                return _dict1.TryAdd(key1, value) && _dict2.TryAdd(key2, value);
+            }
+            finally
+            {
+                _rwLock.ExitWriteLock();
             }
         }
 
         public bool TryRemove(TKey1 key1, TKey2 key2, out TValue value)
         {
-            lock (this)
+            _rwLock.EnterWriteLock();
+
+            try
             {
-                return dict1.TryRemove(key1, out value) && dict2.TryRemove(key2, out _);
+                return _dict1.Remove(key1, out value) && _dict2.Remove(key2, out _);
+            }
+            finally
+            {
+                _rwLock.ExitWriteLock();
             }
         }
     }
