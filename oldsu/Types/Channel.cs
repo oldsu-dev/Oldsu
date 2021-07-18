d6b45e8f5362aa0e0e9bcf662f6943fb4e0510e1
jeesusmies <burnerthingxd@gmail.com>
2021-07-18 15:45:41 +0300

    Add Channels into Server.

---
+++ oldsu/Types/Channel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Oldsu.Enums;
+
+namespace Oldsu.Types
+{
+    [Keyless]
+    public class Channel
+    {
+        public string Tag { get; set; }
+        public string Topic { get; set; }
+        
+        public bool AutoJoin { get; set; }
+        public bool CanWrite { get; set; }
+
+        public Privileges RequiredPrivileges { get; set; }
+    }
+}
