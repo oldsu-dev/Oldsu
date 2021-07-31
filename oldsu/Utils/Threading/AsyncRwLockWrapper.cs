8e44b29f5394fb3f9b3f206afd51bbac8c6f361c
jiniux <53437303+jiniux@users.noreply.github.com>
2021-07-31 13:13:40 +0200

    Add AsyncRwLockGuard, Action enum and change namespace for LoginResult

--- oldsu/Utils/Threading/AsyncRwLockWrapper.cs
+++ oldsu/Utils/Threading/AsyncRwLockWrapper.cs
@@ -5,10 +5,29 @@ using Nito.AsyncEx;
 
 namespace Oldsu.Utils.Threading
 {
+    public class AsyncRwLockGuard<T> : IDisposable
+    {
+        private IDisposable _rwLock;
+        public T Value { get; }
+
+        public static T operator ~(AsyncRwLockGuard<T> guard) => guard.Value;
+        
+        internal AsyncRwLockGuard(IDisposable rwLock, T value)
+        {
+            _rwLock = rwLock;
+            Value = value;
+        }
+        
+        public void Dispose()
+        {
+            _rwLock.Dispose();
+        }
+    }
+    
     public class AsyncRwLockWrapper<T>
     {
         private readonly AsyncReaderWriterLock _rwLock;
-        private readonly T _value;
+        private T _value;
 
         public AsyncRwLockWrapper(T value)
         {
@@ -16,6 +35,15 @@ namespace Oldsu.Utils.Threading
             _rwLock = new AsyncReaderWriterLock();
         }
 
+        public async Task<AsyncRwLockGuard<T>> AcquireWriteLockGuard() => new(await _rwLock.WriterLockAsync(), _value);
+        public async Task<AsyncRwLockGuard<T>> AcquireReadLockGuard() => new(await _rwLock.ReaderLockAsync(), _value);
+
+        public async Task SetValueAsync(T value)
+        {
+            using (await _rwLock.WriterLockAsync())
+                _value = value;
+        }
+        
         public async Task<TResult> ReadAsync<TResult>(Func<T, TResult> fn)
         {
             using (await _rwLock.ReaderLockAsync())
