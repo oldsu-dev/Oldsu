1185640ad668322a760031a165ce50c6f4bf82ec
jiniux <j3nial@icloud.com>
2021-08-03 22:32:21 +0200

    Renamed User to UserInfo

--- oldsu/Types/Rating.cs
+++ oldsu/Types/Rating.cs
@@ -6,7 +6,7 @@ namespace Oldsu.Types
     public class Rating
     {
         public int UserID { get; set; }
-        public virtual User User { get; set; }
+        public virtual UserInfo User { get; set; }
 
         public int BeatmapID { get; set; }
         public virtual Beatmap Beatmap { get; set; }
