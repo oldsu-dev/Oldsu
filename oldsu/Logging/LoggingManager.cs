9795ef328c317d544821dce74e401d87d95b4d7d
jeesusmies <burnerthingxd@gmail.com>
2021-08-18 18:11:29 +0300

    Add boilerplate logging code, ILoggerWriterStrategy, LoggingManager and MongoDbWriter.

---
+++ oldsu/Logging/LoggingManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Oldsu.Logging.Strategies;
+
+namespace Oldsu.Logging
+{
+    public class LoggingManager
+    {
+        private readonly ILoggerWriterStrategy _logWriterTarget;
+
+        public LoggingManager(ILoggerWriterStrategy logTarget)
+        {
+            _logWriterTarget = logTarget;
+        }
+        
+        public async Task LogInfo<T>(
+            string message, 
+            Exception? exception,
+            object? dump,
+            [CallerLineNumber] int lineNumber = 0,
+            [CallerMemberName] string caller = null)
+        {
+            if (caller == null) throw new ArgumentNullException(nameof(caller));
+            
+            await _logWriterTarget.LogInfo<T>(message, exception, dump, lineNumber, caller);
+        }
+
+        public async Task LogCritical<T>(
+            string message, 
+            Exception? exception,
+            object? dump,
+            [CallerLineNumber] int lineNumber = 0,
+            [CallerMemberName] string caller = null)
+        {
+            if (caller == null) throw new ArgumentNullException(nameof(caller));
+
+            await _logWriterTarget.LogCritical<T>(message, exception, dump, lineNumber, caller);
+        }
+        
+        public async Task LogFatal<T>(
+            string message, 
+            Exception? exception,
+            object? dump,
+            [CallerLineNumber] int lineNumber = 0,
+            [CallerMemberName] string caller = null)
+        {
+            if (caller == null) throw new ArgumentNullException(nameof(caller));
+
+            await _logWriterTarget.LogFatal<T>(message, exception, dump, lineNumber, caller);
+        }
+    }
+}
