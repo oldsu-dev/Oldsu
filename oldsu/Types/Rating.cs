ea2bd93024d2a605dfdb77690130998912304531
jeesusmies <burnerthingxd@gmail.com>
2021-08-22 19:55:00 +0300

    Add key and foreign key specifications to Rating.

--- oldsu/Types/Rating.cs
+++ oldsu/Types/Rating.cs
@@ -1,15 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
 namespace Oldsu.Types
 {
-    [Keyless]
     public class Rating
     {
-        public int UserID { get; set; }
+        [Key]
+        public uint UserID { get; set; }
+        [ForeignKey("UserID")]
         public virtual UserInfo User { get; set; }
-
-        public int BeatmapID { get; set; }
-        public virtual Beatmap Beatmap { get; set; }
+        
+        public int BeatmapsetID { get; set; }
+        [ForeignKey("BeatmapsetID")]
+        public virtual Beatmapset Beatmapset { get; set; }
 
         public float Rate { get; set; }
     }
