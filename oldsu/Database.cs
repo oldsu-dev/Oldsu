b57356018d3c416a2d24edf5e91beaec857e5a11
jiniux <53437303+jiniux@users.noreply.github.com>
2021-08-22 20:34:28 +0200

    Switch password hashing to Bcrypt, add AuthenticationPair.

--- oldsu/Database.cs
+++ oldsu/Database.cs
@@ -4,40 +4,66 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Oldsu.Enums;
+using Pomelo.EntityFrameworkCore.MySql.Storage.Internal;
 
 namespace Oldsu
 {
     public class Database : DbContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
             options.UseMySql(
-                    Environment.GetEnvironmentVariable("OLDSU_DB_CONNECTION_STRING")!,
-                    MySqlServerVersion.LatestSupportedServerVersion
-                );
-
+                Environment.GetEnvironmentVariable("OLDSU_DB_CONNECTION_STRING")!,
+                MySqlServerVersion.LatestSupportedServerVersion
+            );
+        }
 
         public DbSet<StatsWithRank> StatsWithRank { get; set; }
 
         public Task<StatsWithRank> GetStatsWithRankAsync(uint userId, uint mode) =>
             StatsWithRank.Where(st => st.UserID == userId && st.Mode == (Mode) mode).FirstOrDefaultAsync();
         
-        public async Task<UserInfo?> AuthenticateAsync(string username, string password)
+        public async Task<UserInfo?> AuthenticateAsync(string username, string passwordHash)
         {
-            return await this.UserInfo.FromSqlRaw(
-                "SELECT * FROM UserInfo WHERE Username = {0} and Password = {1}",
-                username, password
-            ).FirstOrDefaultAsync();
-        }
-        
-        public async Task RegisterAsync(string username, string email, string password, byte country) =>
-            await this.Database.ExecuteSqlRawAsync(
-               @"INSERT INTO `UserInfo`
-               (`Username`, `Email`, `Password`, `Country`) 
-               VALUES
-               ({0}, {1}, {2}, {3});",
+            var authenticationPair = await this.AuthenticationPairs
+                .Where(auth => auth.User.Username == username).FirstOrDefaultAsync();
 
-               username, email, password, country
-            );
+            if (authenticationPair == null)
+                return null;
+            
+            if (BCrypt.Net.BCrypt.Verify(passwordHash, authenticationPair.Password))
+                return authenticationPair.User;
+            
+            return null;
+        }
+
+        public async Task RegisterAsync(string username, string email, string passwordHash, byte country)
+        {
+            string bcrypt = BCrypt.Net.BCrypt.HashPassword(passwordHash);
+            
+            var transaction = await this.Database.BeginTransactionAsync();
+
+            try
+            {
+                UserInfo userInfo = new UserInfo {Username = username, Email = email, Country = country};
+                
+                await this.UserInfo.AddAsync(userInfo);
+                await this.SaveChangesAsync();
+
+                await this.AuthenticationPairs.AddAsync(new AuthenticationPair
+                    {UserID = userInfo.UserID, Password = bcrypt});
+                
+                await this.SaveChangesAsync();
+                await transaction.CommitAsync();
+            }
+            catch
+            {
+                await this.Database.RollbackTransactionAsync();
+                throw;
+            }
+            
+            
+        }
 
         public async Task AddStatsAsync(uint userid, byte gamemode) =>
             await this.Database.ExecuteSqlRawAsync(
@@ -79,7 +105,9 @@ namespace Oldsu
         public DbSet<BeatmapWithScoreCount> BeatmapsWithScoreCount { get; set; }
         public DbSet<Beatmapset> Beatmapsets { get; set; }
         public DbSet<Rating> Ratings { get; set; }
-        
+
+        public DbSet<AuthenticationPair> AuthenticationPairs { get; set; }
+
         public DbSet<ScoreRow> Scores { get; set; }
         public DbSet<HighScoreWithRank> HighScoresWithRank { get; set; }
 
