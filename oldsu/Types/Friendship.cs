754de2eb0ef8bbb4823c4539b1ccd9b16f352dc8
jiniux <j3nial@icloud.com>
2021-08-23 12:48:12 +0200

    Fix adding friend makes server crash.

--- oldsu/Types/Friendship.cs
+++ oldsu/Types/Friendship.cs
@@ -2,15 +2,14 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
-namespace Oldsu.Types {
-    [Keyless]
-    public class Friendship {
-
-        public uint UserID { get; set; }
+namespace Oldsu.Types
+{
+    public class Friendship
+    {
+        [Key] public uint UserID { get; set; }
         public uint FriendUserID { get; set; }
-        [ForeignKey(nameof(UserID))]
-        public UserInfo User { get; set; }
-        [ForeignKey(nameof(FriendUserID))]
-        public UserInfo FriendUser { get; set; }
+        
+        [ForeignKey(nameof(UserID))] public UserInfo User { get; set; }
+        [ForeignKey(nameof(FriendUserID))] public UserInfo FriendUser { get; set; }
     }
 }
