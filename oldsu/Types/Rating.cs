18b90285956041a75fa86c2f373ae395a20446d5
jeesusmies <burnerthingxd@gmail.com>
2021-08-24 21:45:14 +0300

    Make BeatmapsetID the key of Rating.

--- oldsu/Types/Rating.cs
+++ oldsu/Types/Rating.cs
@@ -6,9 +6,8 @@ namespace Oldsu.Types
 {
     public class Rating
     {
-        [Key]
         public uint UserID { get; set; }
-        public int BeatmapsetID { get; set; }
+        [Key] public int BeatmapsetID { get; set; }
         
         [ForeignKey(nameof(UserID))] public virtual UserInfo User { get; set; }
         [ForeignKey(nameof(BeatmapsetID))] public virtual Beatmapset Beatmapset { get; set; }
