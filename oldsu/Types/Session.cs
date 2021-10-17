e0dd0bf3ffdc87aa85e938c33048f78b18036f95
jeesusmies <23-jeesusmies@users.noreply.gitlab.ayyeve.xyz>
2021-10-17 15:45:03 +0300

    Change SessionId to Token.

--- oldsu/Types/Session.cs
+++ oldsu/Types/Session.cs
@@ -7,7 +7,7 @@ namespace Oldsu.Types
     public class Session
     {
         [Key]
-        public string SessionId { get; set; }
+        public string Token { get; set; }
         
         public uint UserID { get; set; }
         
