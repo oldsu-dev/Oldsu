d6945ee12d844e27786679c177aec3cf75bda83f
miau <23-jeesusmies@users.noreply.gitlab.ayyeve.xyz>
2022-02-18 16:04:08 +0200

    add(Services): Add IScoreService methods.

--- oldsu/Services/IScoreService.cs
+++ oldsu/Services/IScoreService.cs
@@ -1,7 +1,12 @@
-﻿namespace Oldsu.Services
+﻿using System.Threading.Tasks;
+using Oldsu.Enums;
+using Oldsu.Types;
+
+namespace Oldsu.Services
 {
-    public class IScoreService
+    public interface IScoreService
     {
-        
+        public Task<ScoreRow?> GetScoreAsync(uint userId, Mode mode);
+        public Task AddScoreAsync(uint userid, Mode mode);
     }
 }
