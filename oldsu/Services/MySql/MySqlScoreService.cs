66b982621cd31a7e1cef8f2be21b47d75acdaf3f
miau <23-jeesusmies@users.noreply.gitlab.ayyeve.xyz>
2022-02-18 16:05:15 +0200

    add(Services): Unimplemented MySqlScoreService methods.

--- oldsu/Services/MySql/MySqlScoreService.cs
+++ oldsu/Services/MySql/MySqlScoreService.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Oldsu.Enums;
+using Oldsu.Types;
 
 namespace Oldsu.Services.MySql
 {
-    public class MySqlScoreService : DbContext, IUserService
+    public class MySqlScoreService : DbContext, IScoreService
     {
+        public async Task<ScoreRow?> GetScoreAsync(uint userId, Mode mode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task AddScoreAsync(uint userid, Mode mode)
+        {
+            throw new NotImplementedException();
+        }
+        
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             options.UseMySql(
