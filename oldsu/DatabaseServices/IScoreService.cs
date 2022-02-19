d82b802890df2661d309a2394b75c0c6d649254f
miau <23-jeesusmies@users.noreply.gitlab.ayyeve.xyz>
2022-02-19 19:16:35 +0200

    add(ScoreService): GetHighScoresOnMap Implementation and limit.

--- oldsu/DatabaseServices/IScoreService.cs
+++ oldsu/DatabaseServices/IScoreService.cs
@@ -7,7 +7,7 @@ namespace Oldsu.DatabaseServices
 {
     public interface IScoreService
     {
-        public Task<List<HighScoreWithRank>?> GetHighScoresOnMap(string mapHash, Mode gamemode);
+        public Task<List<HighScoreWithRank>?> GetHighScoresOnMap(string mapHash, Mode gamemode, int limit);
         public Task<HighScoreWithRank?> GetHighScoreOnMap(string mapHash, Mode gamemode, uint userId);
         public Task AddScoreAsync(ScoreRow score);
     }
