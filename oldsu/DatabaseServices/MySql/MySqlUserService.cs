f06114c2d21c506deea26ff6aa576fbd68b0c7b2
miau <23-jeesusmies@users.noreply.gitlab.ayyeve.xyz>
2022-02-19 19:36:56 +0200

    add(UserService): AuthenticateAsync.

--- oldsu/DatabaseServices/MySql/MySqlUserService.cs
+++ oldsu/DatabaseServices/MySql/MySqlUserService.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Oldsu.Types;
 
 namespace Oldsu.DatabaseServices.MySql
 {
     public class MySqlUserService : DbContext, IUserService
     {
+        private DbSet<AuthenticationPair> AuthenticationPairs { get; set; }
+        
+        public async Task<UserInfo?> AuthenticateAsync(string username, string passwordHash)
+        {
+            var authenticationPair = await AuthenticationPairs
+                .Where(auth => auth.User.Username == username).Include(auth => auth.User)
+                .FirstOrDefaultAsync();
+
+            if (authenticationPair == null)
+                return null;
+            
+            if (BCrypt.Net.BCrypt.Verify(passwordHash, authenticationPair.Password))
+                return authenticationPair.User;
+            
+            return null;
+        }
+        
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             options.UseMySql(
@@ -12,5 +32,6 @@ namespace Oldsu.DatabaseServices.MySql
                 MySqlServerVersion.LatestSupportedServerVersion
             );
         }
+
     }
 }
