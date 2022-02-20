6b09116a7934b9d35990c6a1fba2d2f8530b91f3
miau <23-jeesusmies@users.noreply.gitlab.ayyeve.xyz>
2022-02-20 16:03:39 +0200

    add: IOnlineUserRepository and FakeOnlineUserRepository.

---
+++ oldsu/Storage/IOnlineUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Oldsu.Types;
+
+namespace Oldsu.Storage
+{
+    public interface IOnlineUserRepository
+    {
+        public Task<UserInfo?> GetUser(uint id);
+        public Task AddUser(UserInfo userInfo);
+        public Task RemoveUser(uint id);
+    }
+}
