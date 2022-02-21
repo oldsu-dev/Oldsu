04887e9e5f7db3146353e02f19996555f5aad3fd
miau <23-jeesusmies@users.noreply.gitlab.ayyeve.xyz>
2022-02-21 14:10:12 +0200

    add: IPaginator and MySqlEntityFrameworkPaginator.

---
+++ oldsu/Utils/Paginator/IPaginator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Oldsu.Utils.Paginator
+{
+    public interface IPaginator<T>
+    {
+        public Task<List<T>> GetNewestPageAsync(int page);
+    }
+}
